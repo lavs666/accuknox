76dfd78780363cc45c927b231630ec8806a56a2c
lavs666 <149358976+lavs666@users.noreply.github.com>
2024-06-15 15:54:05 +0530

    Add files via upload

---
+++ Accuknox.bash
@@ -0,0 +1,34 @@
+recruitment@hurix.com
+careers.hr@quloi.com                
+shivanib@heaptrace.net
+agilesh@innovat.com.au
+jeedipellisaikrishnarao14@gmail.com
+shaina@opengrowth.com
+
+people@bunnystudio.com
+hrm@indianhires.com
+recruiter@cqlsys.co.uk
+careers@quantaprocess.com
+rajshree_patil@bpointer.com
+jobs@aimleap.com
+hr@centocode.com(mearn stack)
+
+sravanas@wavity.com(python,basic linux sytsem)
+jobs@aimleap.com(fuul stack)
+Careers@neutrinotechsystems.com
+rsaxena@vbeyondapac.com
+hr@stringcode.in
+hrm@indianhires.com
+nidhi.soni@yash.com
+varsha.jha5@cognizant.com(voice process)
+hr@qdevtechnolab.com
+hiringkajal3@gmail.com
+ hr@defteam.com(java)
+nandhini.durai@taramsys.com (testing)
+Narmadha.s@t2innovations.com(testing)
+harneet.anand9@gmail.com(testing)
+ careers@zenzzed.com(basic skills of python)
+calightapacta@alight.com(s.w)
+hr@pinkmoontech.com(python)
+careers@thinq24.com 
+careers@suesys.com
